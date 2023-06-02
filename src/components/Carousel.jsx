09d5fbccbdf7c366c506effff4fe41fe22adc610
Oliver Jones <olijones44@hotmail.co.uk>
2023-06-02 17:46:20 +0100

    carousel improvement

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,67 +1,58 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { EffectCoverflow } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
 
 function Carousel() {
-	const [shows, setShows] = useState([{ title: 'Loading...', image: '' }]);
-	const [isDown, setIsDown] = useState(false);
-	const [startX, setStartX] = useState();
-	const [scrollLeft, setScrollLeft] = useState();
+	const [slides, setSlides] = useState([{ title: 'Loading...', image: '' }]);
+	const [loading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		fetch('/tvShows.json')
 			.then((res) => res.json())
 			.then((data) => {
-				setShows(data);
+				setSlides(data);
+				setIsLoading(false);
 			})
 			.catch((err) => console.log(err));
 	}, []);
 
-	function mouseDown(e) {
-		e.preventDefault();
-		setIsDown(true);
-		const slider = e.currentTarget;
-		slider.classList.add('dragging');
-		setStartX(e.pageX - slider.offsetLeft);
-		setScrollLeft(slider.scrollLeft);
-	}
-
-	function mouseUp(e) {
-		e.preventDefault();
-		setIsDown(false);
-		e.currentTarget.classList.remove('dragging');
-	}
-
-	function mouseMove(e) {
-		if (!isDown) return;
-		e.preventDefault();
-		const slider = e.currentTarget;
-		const x = e.pageX - slider.offsetLeft;
-		const walk = (x - startX) * 2.5;
-		slider.scrollLeft = scrollLeft - walk;
-	}
-
 	return (
-		<>
-			<h2 className='heading faq-heading'>Our Favourite Picks</h2>
-			<div
-				className='carousel'
-				onMouseDown={mouseDown}
-				onMouseUp={mouseUp}
-				onMouseLeave={mouseUp}
-				onMouseMove={mouseMove}
-			>
-				{shows.map((show, i) => {
-					return (
-						<img
-							className='card'
-							onMouseDown={mouseDown}
-							onMouseUp={mouseUp}
-							src={show.image}
-						/>
-					);
-				})}
-			</div>
-		</>
+		!loading && (
+			<section className='carousel'>
+				<h2 className='heading faq-heading container'>Our Top Picks</h2>
+				<Swiper
+					grabCursor
+					centeredSlides
+					slidesPerView='auto'
+					effect='coverflow'
+					loop
+					coverflowEffect={{
+						rotate: 50,
+						stretch: 0,
+						depth: 100,
+						modifier: 1,
+						slideShadows: true,
+					}}
+					modules={[EffectCoverflow]}
+				>
+					<div className='swiper-wrapper'>
+						{slides.map((slide, i) => {
+							return (
+								<SwiperSlide
+									key={i}
+									style={{
+										backgroundImage: `url(${slide.image})`,
+									}}
+								/>
+							);
+						})}
+						;
+					</div>
+				</Swiper>
+			</section>
+		)
 	);
 }
 
